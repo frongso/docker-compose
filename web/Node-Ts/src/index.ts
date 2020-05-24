9b35ad6b396714b82d15de1a2fb939e174a063db
frongso <frong.so@hotmail.com>
2020-05-24 19:43:58 +0700

    test

--- web/Node-Ts/src/index.ts
+++ web/Node-Ts/src/index.ts
@@ -1,19 +1,14 @@
 import { LogMiddleware } from './routes/middleware/log.middleware';
-import { UserController } from './routes/controller/user.controller';
 import bodyParser from 'body-parser';
 import { createExpressServer } from 'routing-controllers';
 import { TestController } from './routes/controller/test.controller';
 import { createConnection } from 'typeorm';
-import { RoleController } from './routes/controller/role.controlller';
 import { port } from './config/server';
-import { SecurityController } from './routes/security/security.controller';
-import { CheckJwtMiddleware } from './routes/middleware/checkJwt.middleware';
-import { ListController } from './routes/controller/list.controlller';
 createConnection()
   .then((connection) => {
     const app = createExpressServer({
-      controllers: [TestController, UserController, RoleController, SecurityController, ListController],
-      // middlewares: [LogMiddleware, CheckJwtMiddleware],
+      controllers: [TestController],
+      middlewares: [LogMiddleware],
       routePrefix: '/service',
     });
 
